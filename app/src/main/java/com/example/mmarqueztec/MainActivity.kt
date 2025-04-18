bfbe3f13a2354a6c633de219de8bf960b2f6f56b
Mage0w0 <mattiasmarquez21@gmail.com>
2025-04-17 20:56:24 -0500

    Revert "commit con errores a propósito"
    This reverts commit e405d5675d0a84a5f1f2ddec079429ee8abc0bf2.

--- app/src/main/java/com/example/mmarqueztec/MainActivity.kt
+++ app/src/main/java/com/example/mmarqueztec/MainActivity.kt
@@ -25,26 +25,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Composable
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-        )
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
